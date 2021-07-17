bb62674aac62e1d69c824d75101b9ecd24f48869
Valeria Behrend <valu-sp@hotmail.com>
2021-07-17 16:03:34 +0200

    list new and create controller ok

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,8 +8,19 @@ class ListsController < ApplicationController
     end
 
     def create
+        @list = List.new(list_params)
+        if @list.save
+            redirect_to lists_path
+        else
+           redirect_to new_list_path
+        end
     end
 
     def show
+    end
+
+    private
+    def list_params
+    params.require(:list).permit(:name)
     end
 end

6fd509d5bf3a0404c5b06bef25740b4eec8128fb
Valeria Behrend <valu-sp@hotmail.com>
2021-07-20 20:59:05 +0200

    front end index

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -4,8 +4,11 @@ class BookmarksController < ApplicationController
         @bookmark = Bookmark.new(bookmark_params)
         @list = find_params_id
         @bookmark.list = @list
+        @bookmarks = Bookmark.where(list_id: @list.id)
         if @bookmark.save
             redirect_to list_path(@list)
+        else
+            render 'lists/show'
         end
     end
 
